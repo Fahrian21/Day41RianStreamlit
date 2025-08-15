a0eb0f3af136b9c4e36e2f477c473fea9abdd189
Fahrian21 <fahrian1116@gmail.com>
2025-08-15 23:35:29 +0700

    update requirement4

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np
-import pickle
-import plotly.express as px 
-import plotly.graph_objects as go
+import plotly.express as px
 from datetime import datetime, timedelta
 
 # ---- set konfigurasi halaman ----
